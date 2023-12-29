f7f3f83fa0e3a97c33843dc0f08c942ca3731895
Princic-1837592 <andreapri.99@gmail.com>
2023-12-29 13:09:46 +0100

    LICENSE

--- bases.py
+++ bases.py
@@ -1,3 +1,21 @@
+"""
+    Hooty: fast DNA distance matrix
+    Copyright (C) 2024  Andrea Princic & Giacomo Chiappa
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 A = 0x0
 C = 0x1
 G = 0x2
